4871b5ec5f3476fb5c04221f0924a12d9826a265
cyhanyue <46631400+cyhanyue@users.noreply.github.com>
2023-08-08 22:55:00 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def write_string_to_word(text, filename):
     doc.add_paragraph(text)
     doc.save(filename)
 
-def read_docx(file):
+def read_pdf(file):
     doc = Document(file)
     text = ' '.join([paragraph.text for paragraph in doc.paragraphs])
     return text
