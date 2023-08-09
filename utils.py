1b9bd0b1d75c4fd911a7d317583e47c994982afe
cyhanyue <46631400+cyhanyue@users.noreply.github.com>
2023-08-08 23:43:03 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
 from docx import Document
-from pypdf import PdfReader
+# from pypdf import PdfReader
 
 def write_string_to_word(text, filename):
     doc = Document()
     doc.add_paragraph(text)
     doc.save(filename)
 
-def read_pdf(file):
-    doc = PdfReader(file)
-    text = ' '.join([paragraph.text for paragraph in doc.paragraphs])
-    return text
+# def read_pdf(file):
+#     doc = PdfReader(file)
+#     text = ' '.join([paragraph.text for paragraph in doc.paragraphs])
+#     return text
