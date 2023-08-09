925af61bcf9beaa4fbbd97e82a0828d57bd0e78f
cyhanyue <46631400+cyhanyue@users.noreply.github.com>
2023-08-08 23:55:31 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 from docx import Document
 from pypdf import PdfReader
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+
+def text_to_doc_splitter(text: str):
+    spliiter = RecursiveCharacterTextSplitter(chunk_size = 10000, chunk_overlap  = 0, length_function = len, add_start_index = True,)
+    document = spliiter.create_documents([text])
+    return document
 
 def write_string_to_word(text, filename):
     doc = Document()
@@ -7,6 +13,10 @@ def write_string_to_word(text, filename):
     doc.save(filename)
 
 def read_pdf(file):
-    doc = PdfReader(file)
-    text = ' '.join([paragraph.text for paragraph in doc.paragraphs])
-    return text
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    
+    document = text_to_doc_splitter(text)
+    return document
