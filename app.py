d4aa9a06f64bef6d8d0deadae877822d0b87eba1
cyhanyue <46631400+cyhanyue@users.noreply.github.com>
2023-08-09 23:18:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def build_streamlit_app():
     st.title('Outreach Email Generator')
 
     # Create an input box in the sidebar of the app for the OpenAI API key
-    st.text('Get an OpenAI API key here: https://help.openai.com/en/collections/3675940-getting-started-with-openai-api')
-    st.text('Paste temporary OpenAI API key to the left: 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')
+    st.text('Get an OpenAI API key here: \n https://help.openai.com/en/collections/3675940-getting-started-with-openai-api')
+    st.text('Paste temporary OpenAI API key to the left: \n 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')
     
     openai_api_key = os.getenv("OPENAI_API_KEY")
     if openai_api_key is None:
